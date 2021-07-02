ea7326babb0b451d7a6ff5dc57ae7a803487407d
muzammil-aslam <muzammilaslam914@gmail.com>
2021-07-01 22:09:11 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,89 @@
+// var name = prompt('wear are you from')
+// // console.log(name)
+// var age = ("muzammil aslam");
+// var age = ('kamran bhai');
+// alert(age)
+
+// var age = prompt ('enter your age');
+// var name = prompt('enter your live');
+// var went = prompt('what dou you went to be');
+// var dou = prompt('which game do you like');
+// var bessfriend = prompt('how is you bess friend');
+
+// console.log(age)
+// console.log(name)
+// console.log(went)
+// console.log(dou)
+// console.log(bessfriend)
+
+// var myarry = ['mango','banana','musembi','orange'];
+
+// console.log(myarry)
+// console.log(myarry[2],myarry[0],myarry[3])
+
+// var math = 1000
+// var urdu = 1000
+// var english = 1000
+// var islamiyat = 1000
+// var psp = 1000
+
+// var aslam = math + urdu + english + islamiyat + psp
+
+// alert(aslam)
+
+// alert(urdu)
+
+// var username = 'kamran'
+// var as = 'aslam'
+// alert(username)
+// alert(as)
+
+// var myarry = ["mango", "banan", "kamran", "123", "true", "fals" ,'hamad'];
+// console.log(myarry);
+// console.log(myarry.push('jamal'));
+// console.log(myarry[4],myarry[2],myarry[5]);
+// var number = 20;
+// var newnumber = 10;
+// var total = number + newnumber;
+// alert(total);
+// var x = 18;
+
+// if (x > 19) {
+//   document.write("clear");
+// } else {
+//   document.write("noteclear");
+// }
+
+// var age = "100";
+// if (age > 90) {
+//   document.write("clear");
+// } else {
+//   document.write("note clear");
+// }
+// var num = 1;
+// var newNum = num--;
+// alert(newNum - num);
+// var age = 20;
+
+// if (age < 10 ){
+//   console.log('usama');
+// } else {
+//     console.log('aslam')
+// }
+// for(var i=0; i<20; i++)
+//  document.write(i)
+
+// var a = ['a', 'b', 'c', 'd', 'e','f']
+// var b = [1 ,2 ,3 ,4 ,5]
+// for (var i=0; i<5; i++){
+//     document.write(a(i))
+// var age;
+// age = "12";
+// alert(age);
+// var number =  5000 * 2;
+// alert(number);
+// var num = 10;
+// var popularnumber = num + 200;
+// alert(popularnumber)
+ 
+
